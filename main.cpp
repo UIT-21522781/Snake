c7abff17c163a755a9792990fd28a25bf3e8b112
UIT-21522781 <21522781@gm.uit.edu.vn>
2022-11-24 18:58:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -62,24 +62,6 @@ void showStartMenu();
 void drawBox();
 #pragma endregion
 
-void drawBox()
-{
-	for (size_t i = 0; i < WIDTH; i++)
-		cout << '=';
-	gotoxy(0, HEIGHT);
-	for (size_t i = 0; i < WIDTH; i++)
-		cout << '=';
-	for (size_t i = 1; i < HEIGHT; i++)
-	{
-		gotoxy(0, i);
-		cout << '|';
-	}
-	for (size_t i = 1; i < HEIGHT; i++)
-	{
-		gotoxy(WIDTH, i);
-		cout << '|';
-	}
-}
 int main()
 {
 	showStartMenu();
