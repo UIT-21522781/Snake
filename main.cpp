e92679674a9ea9ef84be8c6417989ef81a2972c2
UIT-21522413 <118763742+UIT-21522413@users.noreply.github.com>
2022-11-25 01:19:25 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -206,30 +206,4 @@ void displayScore()
 }
 
 
-bool isAteApple()
-{
-	return snake[0].x == apple.x && snake[0].y == apple.y;
-}
 
-void genApple()
-{
-	srand(time(0));
-	int x = rand() % (WIDTH - 1) + 1;
-	int y = rand() % (HEIGHT - 1) + 1;
-	apple = {
-		x,
-		y,
-	};
-	gotoxy(x, y);
-	cout << APPLE;
-}
-void gotoxy(int x, int y)
-{
-	COORD coord;
-	coord.X = x;
-	coord.Y = y;
-	SetConsoleCursorPosition(
-		GetStdHandle(STD_OUTPUT_HANDLE),
-		coord
-	);
-}
