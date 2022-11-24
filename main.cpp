370b20da59768a3cccbaacba240bc5e2fcfc509d
UIT-21522781 <21522781@gm.uit.edu.vn>
2022-11-24 22:07:53 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,91 @@
+#include <iostream>
+using namespace std;
+
+#include <vector>
+#include <windows.h>
+#include <conio.h>
+#include <cstring>
+#include <time.h>
+#include <random>
+
+#define WIDTH 40
+#define HEIGHT 20
+#define BODY 'O'
+#define APPLE '*'
+
+enum class Direction
+{
+	up,
+	right,
+	down,
+	left
+};
+struct Point
+{
+	int x;
+	int y;
+};
+
+#pragma region GlobalVariable
+
+vector<Point> snake = {
+	Point{ WIDTH / 2 + 2, HEIGHT / 2 },
+	Point{ WIDTH / 2 + 1, HEIGHT / 2 },
+	Point{ WIDTH / 2, HEIGHT / 2 },
+	Point{ WIDTH / 2 - 1, HEIGHT / 2 },
+	Point{ WIDTH / 2 - 2, HEIGHT / 2 }
+};
+Direction direction = Direction::right;
+Point apple;
+int score = 0;
+int speed = 300;
+Point prevTail;
+#pragma endregion
+
+#pragma region Prototype
+void gotoxy(int, int);
+void drawSnake();
+void drawSnakePart(Point);
+void ShowConsoleCursor(bool);
+void move();
+void drawHeadnTail();
+void genApple();
+bool isHitWall();
+bool isBiteItself();
+bool isAteApple();
+void growing();
+void displayScore();
+void showEndMenu();
+void startGame();
+void resetSnake();
+void showStartMenu();
+void drawBox();
+#pragma endregion
+
+void drawBox()
+{
+	for (size_t i = 0; i < WIDTH; i++)
+		cout << '=';
+	gotoxy(0, HEIGHT);
+	for (size_t i = 0; i < WIDTH; i++)
+		cout << '=';
+	for (size_t i = 1; i < HEIGHT; i++)
+	{
+		gotoxy(0, i);
+		cout << '|';
+	}
+	for (size_t i = 1; i < HEIGHT; i++)
+	{
+		gotoxy(WIDTH, i);
+		cout << '|';
+	}
+}
+int main()
+{
+	showStartMenu();
+	return 0;
+}
+
 void drawSnake()
 {
 	for (size_t i = 0; i < snake.size(); i++)
@@ -30,4 +118,19 @@ void resetSnake()
 		Point{ WIDTH / 2 - 1, HEIGHT / 2 },
 		Point{ WIDTH / 2 - 2, HEIGHT / 2 }
 	};
+}
+
+void move()
+{
+	prevTail = snake.back();
+	for (size_t i = snake.size() - 1; i > 0; i--)
+		snake[i] = snake[i - 1];
+	if (direction == Direction::up)
+		snake[0].y -= 1;
+	else if (direction == Direction::down)
+		snake[0].y += 1;
+	else if (direction == Direction::left)
+		snake[0].x -= 1;
+	else if (direction == Direction::right)
+		snake[0].x += 1;
 }
